5ed32350dced7e61631f27ff79b5940a061be698
Ali Saka <muhammedsakaa1@gmail.com>
2025-12-25 15:21:07 +0300

    bar ve skor sayfası editlendi

--- src/Scored.jsx
+++ src/Scored.jsx
@@ -1,43 +1,50 @@
 import React, { useEffect, useState } from 'react'
 import App from './App.jsx'
-function Scored() {
-    const [play,setPlay] = useState(false)
-    useEffect(() => {
-    if(play) {
-      location.reload(); 
+
+function Scored({ score, total }) {
+  const [play, setPlay] = useState(false)
+
+  useEffect(() => {
+    if (play) {
+      location.reload()
     }
   }, [play])
-    if(play){
-        return <App />
-    }
+
+  if (play) {
+    return <App />
+  }
+
   return (
     <section className='scorsec'>
-        <div className='scoredWr'>
-            <p>Quiz completed</p>
-            <h1>You scored...</h1>
-        </div>
-        <div className='scoredBox'>
-            <div className='minibox' >
-                <div>
-                    <div className='headerWr' style={{display:'flex'}}>
-                        <img src="src/assets/img/Group 83.png" alt="" />
-                        <h5>Accessibility</h5>
-                    </div>
-                </div>
-                <div>
-                    <h2>8</h2>
-                    <p>out of 10</p>
-                </div>
+      <div className='scoredWr'>
+        <p>Quiz completed</p>
+        <h1>You scored...</h1>
+      </div>
+
+      <div className='scoredBox'>
+        <div className='minibox'>
+          <div>
+            <div className='headerWr' style={{ display: 'flex' }}>
+              <img src="src/assets/img/Group 83.png" alt="" />
+              <h5>Accessibility</h5>
             </div>
-            <div>
-                <button 
-                className='scordbtn'
-                onClick={() => setPlay(true)}
-                >
-                    Play Again
-                </button>
-            </div>
+          </div>
+
+          <div>
+            <h2>{score}</h2>
+            <p>out of {total}</p>
+          </div>
         </div>
+
+        <div>
+          <button
+            className='scordbtn'
+            onClick={() => setPlay(true)}
+          >
+            Play Again
+          </button>
+        </div>
+      </div>
     </section>
   )
 }
